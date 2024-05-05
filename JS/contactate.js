cbde8367f6e7db2b4971e6b6bd3f0461c3f32338
Pablo Rivero <privero@claro.com.ar>
2024-05-05 17:51:41 -0300

    FIX, agrego validaciones en los formulario de eventos y contacto

--- JS/contactate.js
+++ JS/contactate.js
@@ -7,29 +7,32 @@ function validateForm() {
   // Valido que el nombre tenga más de 3 letras y no contenga números ni caracteres especiales
   let namePattern = /^[a-zA-ZáéíóúÁÉÍÓÚ\s]{3,}$/;
   if (!namePattern.test(name)) {
-      alert("El nombre debe tener más de 3 letras y no debe contener números ni caracteres especiales");
-      return false;
+    alert(
+      "El nombre debe tener más de 3 letras y no debe contener números ni caracteres especiales"
+    );
+    return false;
   }
 
   // Valido formato de email
   let emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   if (!emailPattern.test(email)) {
-      alert("Por favor, ingrese un correo electrónico válido");
-      return false;
+    console.log("entre");
+    alert("Por favor, ingrese un correo electrónico válido");
+    return false;
   }
 
   // Valido que el celular contenga solo números y tenga 10 dígitos
   let celularPattern = /^\d{10}$/;
   if (!celularPattern.test(celular)) {
-      alert("El número de celular debe contener solo números y tener 10 dígitos");
-      return false;
+    alert("El número de celular debe contener solo números y tener 10 dígitos");
+    return false;
   }
 
   if (message === "") {
-      alert("Por favor, complete todos los campos");
-      return false;
+    alert("Por favor, complete todos los campos");
+    return false;
   } else {
-  console.log('enviar');
-      document.getElementById("form").submit();
+    console.log("enviar");
+    document.getElementById("form").submit();
   }
 }
